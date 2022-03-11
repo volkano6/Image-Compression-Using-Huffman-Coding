4fc51453e30140f271cf14f051d6b1b0d310a6f8
ceirem <72570708+ceirem@users.noreply.github.com>
2022-03-11 21:09:43 +0300

    v 1.5.1

--- tryToDoIt.py
+++ tryToDoIt.py
@@ -1,5 +1,4 @@
 import fileinput
-import csv
 import numpy
 from PIL import *
 from PIL import Image
@@ -8,6 +7,7 @@ import os
 from numpy import ndarray
 import Huffman
 import math
+import cv2
 
 
 def main():
@@ -62,43 +62,9 @@ def compressionLevel2(file,img):
     convertMatrixToImage(newImgArr)
 
 def convertMatrixToImage(arr, im=None):
-    # create a numpy array from scratch
-    # using arange function.
-    # 1024x720 = 737280 is the amount
-    # of pixels.
-    # np.uint8 is a data type containing
-    # numbers ranging from 0 to 255
-    # and no non-negative integers
-    arrRow = len(arr)
-    arrColmn = len(arr[0])
-    size = arrRow*arrColmn
-    array = np.arange(0, size, 1, np.uint8)
+    #convert 2D array to image
+    cv2.imwrite("newphoto.png",arr)
 
-    # check type of array
-    print(type(array))
-
-    # our array will be of width
-    # 737280 pixels That means it
-    # will be a long dark line
-    print(array.shape)
-
-    # Reshape the array into a
-    # familiar resoluition
-    array = np.reshape(array, (arrColmn, arrRow))
-
-    # show the shape of the array
-    print(array.shape)
-
-    # show the array
-    print(array)
-
-    # creating image object of
-    # above array
-    data = im.fromarray(array)
-
-    # saving the final output
-    # as a PNG file
-    data.save("restoredImg.png")
 
 def strToArr2DWithSpace(str,row, colmn):
     arr = np.fromstring(str, sep=" ", dtype=int)
